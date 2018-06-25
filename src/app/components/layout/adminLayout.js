84513f8f7826f95cad9fc759a1a2caf214805435
teukufadh <tmfadhlul@gmail.com>
2018-06-26 06:11:08 +0700

    fix conflicts

--- src/app/components/layout/adminLayout.js
+++ src/app/components/layout/adminLayout.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import Header from "../header/header"
 export default class adminLayout extends Component {
   render() {
     return (
-      <main className="container">
-        <Header />
+      <main>
         {this.props.children}
       </main>
     )
